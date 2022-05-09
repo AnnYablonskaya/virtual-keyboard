f50c3e7014c87844383c0d240671c90928ffa238
AnnYablonskaya <yaanna1910@gmail.com>
2022-05-09 18:47:08 +0300

    docs: fix buttons

--- js/Key.js
+++ js/Key.js
@@ -9,15 +9,15 @@ export default class Key {
     this.big = big;
     this.isFnKey = Boolean(small.match(/Ctrl|arr|Alt|Shift|Enter|Tab|Back|Win|Del|Caps/));
     if (big && big.match(/[^a-zA-zа-яА-яЁё0-9]/)) {
-      this.sub = main('div', 'sub', this.big);
+      this.sumbol = main('div', 'sumbol', this.big);
     } else {
-      this.sub = main('div', 'sub', '');
+      this.sumbol = main('div', 'sumbol', '');
     }
     this.letter = main('div', 'letter', small);
     this.div = main(
       'div',
       'keyboard__key',
-      [this.sub, this.letter],
+      [this.sumbol, this.letter],
       null,
       ['code', this.code],
       this.isFnKey ? ['fn', 'true'] : ['fn', 'false'],
