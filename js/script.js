25881ea655038c6958e5904efc9d3058bd573eb2
AnnYablonskaya <yaanna1910@gmail.com>
2022-05-09 22:03:14 +0300

    docs: update files

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ import Keyboard from './Keybord.js';
 alert('Доброго времени суток, уважаемые и глубоколюбимые проверяющие! По многим личным причинам не смогла доделать клавиатуру до дедлайна. Прошу Вас дать мне время доделать и проверить работу в четверг во второй половине дня. Спасибо большое за ваше понимание.!');
 
 const rows1 = [
-  ['letter', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9', 'num0', 'minus', 'equal', 'delete'],
-  ['tab', 'letQ', 'letW', 'letE', 'letR', 'letT', 'letY', 'letU', 'letI', 'letO', 'letP', 'bracketLeft', 'bracketRight', 'backspace'],
-  ['capsLock', 'letA', 'letS', 'letD', 'letF', 'letG', 'letH', 'letJ', 'letK', 'letL', 'semicolon', 'Quote', 'backslash', 'enter'],
-  ['shiftLeft', 'intlBackslash', 'letZ', 'letX', 'letC', 'letV', 'letB', 'letN', 'letM', 'comma', 'dot', 'slash', 'arrowUp', 'shiftRight'],
-  ['controlLeft', 'win', 'altLeft', 'space', 'altRight', 'arrowLeft', 'arrowDown', 'arrowRight', 'controlRight'],
+  ['Letter', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Delete'],
+  ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backspace'],
+  ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Backslash', 'Enter'],
+  ['ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Dot', 'Slash', 'ArrowUp', 'ShiftRight'],
+  ['ControlLeft', 'Win', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
 ];
 const lang = get('lang', '"ru"');
 new Keyboard(rows1).init(lang).generateLays();
