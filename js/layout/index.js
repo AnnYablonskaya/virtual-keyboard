f073002a1d518cf9e4b20c99b18e9a8f7a397f56
AnnYablonskaya <92150346+AnnYablonskaya@users.noreply.github.com>
2022-05-09 21:09:49 +0300

    Delete js directory

--- js/layout/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable import/extensions */
-
-import ru from './ru.js';
-import eng from './eng.js';
-
-export default { ru, eng };
--- js/layout/storage.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-export function set(name, value) {
-  window.localStorage.setItem(name, JSON.stringify(value));
-}
-
-export function get(name, sumbol = null) {
-  return JSON.parse(window.localStorage.getItem(name) || sumbol);
-}
